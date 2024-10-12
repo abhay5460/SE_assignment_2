78c7f271e4c7159ca3d8b5efabbc0e7c9b646a9c
Abhay Gajjar <gajjarabhay82@gmail.com>
2024-10-12 19:17:46 +0530

    Update Accept_5_number_find_factorial.c

--- Looping and Conditional statement programs/Accept_5_number_find_factorial.c
+++ Looping and Conditional statement programs/Accept_5_number_find_factorial.c
@@ -3,14 +3,20 @@
 #include<stdio.h>
 int main()
 {
-	int i , n , sum=1;
+	int i , n , j;
 	
 	for(i=1;i<=5;i++)
 	{
 		printf("Enter your number : ");
 		scanf("%d",&n);
 		
-		sum = sum * n;
+		int sum=1;
+		for(j=1;j<=n;j++)
+		{
+			sum *= j;
+		}
+			
+		printf("Factorial is : %d",sum);
+		printf("\n");
 	}
-	printf("Factorial is : %d",sum);
 }
